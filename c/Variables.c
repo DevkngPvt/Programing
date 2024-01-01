bd039b27e8b493ef659630d35e2d5229d96a6b3a
devkng <dkprojects121@gmail.com>
2024-01-01 11:11:49 -0800

    Variables

--- c/Variables.c
+++ c/Variables.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-// Varibles 
+// Variables
 /*
 => To Allocate space in memory to store a value
-=> We refers to a varible's name to acess the stored value
-=> That varibles now behaves as if  it eas the value it contains 
+=> We refers to a variable's name to access the stored value
+=> That variable's now behaves as if  it eas the value it contains
 => But we need to declare what type of data we are storing.
 */
 
 int main() {
     int x ; // declaration
-    x = 5 ; // initization
-    int y = 123 ; // declaration + initization
+    x = 5 ; // initialization
+    int y = 123 ; // declaration + initialization
 
     int age = 21 ; // integer
     float gpa = 2.05 ; // floating point number
-    char grade = 'C' ; // single charater
-    char name[] = "Bro"; //array of charaters
+    char grade = 'C' ; // single character
+    char name[] = "Bro"; //array of characters
 
     printf("You are %d years old\n",age);
     printf("Your gpa is %f \n",gpa);
