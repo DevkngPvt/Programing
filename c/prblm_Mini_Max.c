403369041574163a93239db91aa0806bc71e1285
devkng <dkprojects121@gmail.com>
2024-01-03 16:54:39 -0800

    Version 10

---
+++ c/prblm_Mini_Max.c
@@ -0,0 +1,44 @@
+// Problem 1
+
+//            ----------------------------------
+//                   => Min Max Problem
+//            ----------------------------------
+
+/* => Question 
+  -------------   
+ Given five positive integers, 
+ find the minimum and maximum values that can be calculated 
+ by summing exactly four of the five integers. 
+ Then print the respective minimum and 
+ maximum values as a single line of two space-separated long inter
+
+*/
+
+/*
+
+Example
+arr = [1, 3, 5, 7, 9]
+The minimum sum is 1+3+5+ 7 = 16 
+The maximum sum is 3+5+7+9= 24. The function prints
+
+*/
+
+// Source => https://youtu.be/U6LDKYwJKTA?si=pvw5_TotdSKEqczh
+
+//               Answer  
+//          ----------------
+
+#include<stdio.h>
+
+void main() {
+
+     int n;
+     
+     scanf("Enter the array size : ");
+     scanf("%d", &n) ;
+
+     for(int i = 0 ; i < n ; i ++)
+     {
+        
+     }
+}
